8430a90f5e471d217e15ad05aee77b632ec310fe
Jérémy Hoarau <jeremyhoarau44@gmail.com>
2025-01-17 12:58:44 +0400

    Nouveau fichier migration correspondant au nouveau schema de données.

---
+++ migrations/Version20250117084840.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250117084840 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE evenement DROP INDEX UNIQ_B26681ED936B2FA, ADD INDEX IDX_B26681ED936B2FA (organisateur_id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE evenement DROP INDEX IDX_B26681ED936B2FA, ADD UNIQUE INDEX UNIQ_B26681ED936B2FA (organisateur_id)');
+    }
+}
