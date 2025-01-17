d13b90f5403c6abae643e20d17c41f7e7e243a5e
Jérémy Hoarau <jeremyhoarau44@gmail.com>
2025-01-17 15:19:39 +0400

    Ajout du controller et du fichier test de Home pour l'api

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\EvenementRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Serializer\SerializerInterface;
+
+final class HomeController extends AbstractController
+{
+    #[Route('/', name: 'app_home')]
+    public function index(): Response
+    {
+        return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+
+    #[Route(path:'/api', name:'app_evenement_api', methods: ['GET'])]
+    public function getEvenements(SerializerInterface $serializer, EvenementRepository $evenement): JsonResponse
+    {
+        $data = $serializer->serialize($evenement->findAll(), 'json', ['groups' => 'evenement:list']);
+
+        return $this->json([
+            'messages' => "Success",
+            'code' => 200,
+            'data' => json_decode($data)
+        ], 200);
+    }
+}
