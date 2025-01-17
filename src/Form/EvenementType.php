55eed8bff5258125572c1333ec379b262a1c0c72
Jérémy Hoarau <jeremyhoarau44@gmail.com>
2025-01-17 11:19:02 +0400

    Ajout du CRUD pour Evenement

---
+++ src/Form/EvenementType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Evenement;
+use App\Entity\Utilisateur;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class EvenementType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('dateEvenement', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('organisateur', EntityType::class, [
+                'class' => Utilisateur::class,
+                'choice_label' => 'id',
+            ])
+            ->add('participant', EntityType::class, [
+                'class' => Utilisateur::class,
+                'choice_label' => 'id',
+                'multiple' => true,
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Evenement::class,
+        ]);
+    }
+}
