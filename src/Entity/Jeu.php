8e1f75cd248786a2ed63f24630aac4509a6c653e
Jérémy Hoarau <jeremyhoarau44@gmail.com>
2025-01-17 09:38:06 +0400

    Création de l'entitté et du repository Jeu

---
+++ src/Entity/Jeu.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\JeuRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: JeuRepository::class)]
+class Jeu
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $nom = null;
+
+    #[ORM\Column]
+    private ?int $nbParticipant = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): static
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getNbParticipant(): ?int
+    {
+        return $this->nbParticipant;
+    }
+
+    public function setNbParticipant(int $nbParticipant): static
+    {
+        $this->nbParticipant = $nbParticipant;
+
+        return $this;
+    }
+}
